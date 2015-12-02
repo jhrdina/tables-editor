1f1c7d6ae7e335836b487a7ade61f02829d82405
stepan <granat.stepan@gmail.com>
2015-12-02 20:42:09 +0100

    Curve drawing now working

--- app/elements/connection-view/connection-view.ts
+++ app/elements/connection-view/connection-view.ts
@@ -3,24 +3,30 @@
 class Coords {
   constructor(public x: number = 0, public y: number = 0) {
   }
+  getCenteredBy(p1: Coords, space: number = 0) {
+    var p2 = new Coords(this.x, this.y);
+    p2.x = p2.x - p1.x + space;
+    p2.y = p2.y - p1.y + space;
+    return p2;
+  }
 }
 
-function lineTrim(p1: Coords, p2: Coords) {
+function lineTrim(p1: Coords, p2: Coords, space: number = 0) {
   var line = {p1: new Coords(), p2: new Coords()};
   if (p1.x < p2.x) {
-    line.p1.x = 0;
-    line.p2.x = p2.x - p1.x;
+    line.p1.x = space;
+    line.p2.x = (p2.x - p1.x) + space;
   } else {
-    line.p2.x = 0;
-    line.p1.x = p1.x - p2.x;
+    line.p2.x = space;
+    line.p1.x = (p1.x - p2.x) + space;
   }
 
   if (p1.y < p2.y) {
-    line.p1.y = 0;
-    line.p2.y = p2.y - p1.y;
+    line.p1.y = space;
+    line.p2.y = (p2.y - p1.y) + space;
   } else {
-    line.p2.y = 0;
-    line.p1.y = p1.y - p2.y;
+    line.p2.y = space;
+    line.p1.y = (p1.y - p2.y) + space;
   }
   return line;
 }
@@ -42,11 +48,31 @@ class Curve {
     this.p2 = p2;
     this.end = end;
   }
+  toString() {
+    var str: String;
+
+    var p1 = this.p1.getCenteredBy(this.start);
+    var middle = this.middle.getCenteredBy(this.start);
+
+    str = "M " + this.start.x + " " + this.start.y + " q ";
+    str += p1.x + " " + p1.y + " ";
+    str += middle.x + " " + middle.y + " ";
+
+    var p2 = this.p2.getCenteredBy(this.middle);
+    var end = this.end.getCenteredBy(this.middle);
+
+    str += "M " + this.middle.x + " " + this.middle.y + " q ";
+    str += p2.x + " " + p2.y + " ";
+    str += end.x + " " + end.y + " ";
+
+    return str;
+  }
 }
 
 @component('connection-view')
 class ConnectionView extends polymer.Base {
-  static space = 30;
+  static space = 40;
+  static angulation = 30;
 
   @property({ type: Number, notify: true })
   x1: number;
@@ -71,68 +97,48 @@ class ConnectionView extends polymer.Base {
     var frame = this.$.frame;
 
     if (x1 < x2) {
-      frame.style.left = x1;
+      frame.style.left = (x1 - ConnectionView.space) + "px";
     } else {
-      frame.style.left = x2;
+      frame.style.left = (x2 - ConnectionView.space) + "px";
     }
 
     if (y1 < y2) {
-      frame.style.top = y1;
+      frame.style.top = (y1 - ConnectionView.space) + "px";
     } else {
-      frame.style.top = y2;
+      frame.style.top = (y2 - ConnectionView.space) + "px";
     }
 
-    frame.setAttribute("width", Math.abs(x1 - x2));
-    frame.setAttribute("height", Math.abs(x1 - x2));
+    frame.setAttribute("width", (Math.abs(x1 - x2) + 2 * ConnectionView.space) + "px");
+    frame.setAttribute("height", (Math.abs(y1 - y2) + 2 * ConnectionView.space) + "px");
   }
 
-  getPath() {
+  @computed({type: Curve})
+  curve(x1, y1, x2, y2, dir1, dir2): Curve {
     var curve = new Curve();
-    var diagonal = lineTrim({ x: this.x1, y: this.y1 }, { x: this.x2, y: this.y2 });
+    var diagonal = lineTrim(new Coords(x1, y1), new Coords(x2, y2), ConnectionView.space);
     curve.start = diagonal.p1;
     curve.end = diagonal.p2;
 
-    curve.p1 = { x: curve.start.x + ConnectionView.space, y: curve.start.y };
-    curve.p2 = { x: curve.end.x - ConnectionView.space, y: curve.end.y };
+    if(dir1 == "left") {
+      curve.p1 = new Coords(curve.start.x - ConnectionView.angulation, curve.start.y);
+    } else {
+      curve.p1 = new Coords(curve.start.x + ConnectionView.angulation, curve.start.y);
+    }
+    if(dir2 == "left") {
+      curve.p2 = new Coords(curve.end.x - ConnectionView.angulation, curve.end.y);
+    } else {
+      curve.p2 = new Coords(curve.end.x + ConnectionView.angulation, curve.end.y);
+    }
 
     curve.middle.x = (curve.p1.x + curve.p2.x) / 2.0;
     curve.middle.y = (curve.p1.y + curve.p2.y) / 2.0;
 
-    var str: String;
-    str = "M " + curve.start.x + " " + curve.start.y + " q ";
-    str += curve.p1.x + " " + curve.p1.y + " ";
-    str += curve.middle.x + " " + curve.middle.y + " ";
-
-    str += "M " + curve.middle.x + " " + curve.middle.y + " q ";
-    str += (curve.p2.x - curve.middle.x) + " " + (curve.p2.y - curve.middle.y) + " ";
-    str += (curve.end.x - curve.middle.x) + " " + (curve.end.y - curve.middle.y) + " ";
-
-    return str;
+    return curve;
   }
 
   @computed({type: String})
-  curvePath(x1, y1, x2, y2, dir1, dir2): String {
-    var curve = new Curve();
-    var diagonal = lineTrim({ x: x1, y: y1 }, { x: x2, y: y2 });
-    curve.start = diagonal.p1;
-    curve.end = diagonal.p2;
-
-    curve.p1 = { x: curve.start.x + ConnectionView.space, y: curve.start.y };
-    curve.p2 = { x: curve.end.x - ConnectionView.space, y: curve.end.y };
-
-    curve.middle.x = (curve.p1.x + curve.p2.x) / 2.0;
-    curve.middle.y = (curve.p1.y + curve.p2.y) / 2.0;
-
-    var str: String;
-    str = "M " + curve.start.x + " " + curve.start.y + " q ";
-    str += curve.p1.x + " " + curve.p1.y + " ";
-    str += curve.middle.x + " " + curve.middle.y + " ";
-
-    str += "M " + curve.middle.x + " " + curve.middle.y + " q ";
-    str += (curve.p2.x - curve.middle.x) + " " + (curve.p2.y - curve.middle.y) + " ";
-    str += (curve.end.x - curve.middle.x) + " " + (curve.end.y - curve.middle.y) + " ";
-
-    return str;
+  curveString(curve): String{
+    return curve.toString();
   }
 }
 
