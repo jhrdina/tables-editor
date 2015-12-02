6fedfc67e288dba7e87e46ee92d72cc411337050
Jan Hrdina <jan.hrdka@gmail.com>
2015-12-02 22:18:57 +0100

    EntityBox: Constraint and datatype badges

---
+++ app/elements/constraint-badge/constraint-badge.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../../bower_components/polymer-ts/polymer-ts.d.ts"/>
+
+@component('constraint-badge')
+class ConstraintBadge extends polymer.Base {
+
+  static transformations: any = {
+    'PRIMARY_KEY': 'PK',
+    'FOREIGN_KEY': 'FK',
+    'NUMBER': 'NUM',
+    'NOT_NULL': 'NOT NULL',
+    'VARCHAR': 'VCHAR'
+  }
+
+  @property({ type: Boolean, value: false })
+  noTransform: boolean;
+
+  @property({ type: String })
+  label: string;
+
+  @observe('label')
+  labelChanged(label) {
+    this.setAttribute('aria-label', label);
+  }
+
+  @computed()
+  transformedLabel(label): string {
+    if (this.noTransform) return label;
+
+    return ConstraintBadge.transformations[label] || label;
+  }
+}
+
+ConstraintBadge.register();
