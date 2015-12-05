55c3dc34bcad2e4aec986c993a0574165cdb5507
Jan Hrdina <jan.hrdka@gmail.com>
2015-12-05 20:10:46 +0100

    Entities area: basic connection creating

--- app/elements/entities-area/entities-area.ts
+++ app/elements/entities-area/entities-area.ts
@@ -30,16 +30,53 @@ class EntitiesArea extends polymer.Base
     });
   }
 
-  activeIndex: number = -1;
+  // ================================================================
+  // Connection mode
+  // ================================================================
+
+  sourceEntityIndex: number = -1;
+  sourceEntityElement: any;
+  preventEntityTargetSelection: boolean = false;
 
   connectorActiveChanged(e) {
-    this.activeIndex = e.detail.value === true ?
-      this.$.entitiesRepeat.indexForElement(e.target) :
-      -1;
+    if (e.detail.value === true) {
+      this.sourceEntityElement = e.target;
+      this.sourceEntityIndex = this.$.entitiesRepeat.indexForElement(e.target);
+
+      this.preventEntityTargetSelection = true;
+
+    } else {
+      this.sourceEntityElement = null;
+      this.sourceEntityIndex = -1;
+    }
   }
 
-  computeConnectorHidden(index, activeIndex) {
-    return activeIndex !== -1 && index !== activeIndex;
+  computeConnectorHidden(index, sourceEntityIndex) {
+    return sourceEntityIndex !== -1 && index !== sourceEntityIndex;
+  }
+
+  handleEntityTap(e) {
+    // Prevent immediate selecting the same entity as a Target
+    // when tapping the connector button.
+    if (this.preventEntityTargetSelection) {
+      this.preventEntityTargetSelection = false;
+      return;
+    }
+
+    var targetEntityIndex = this.$.entitiesRepeat.indexForElement(e.target);
+
+    if (this.sourceEntityIndex === -1) return;
+
+    this.push('model.connections', {
+      entity1: this.sourceEntityIndex,
+      entity2: targetEntityIndex,
+      name: '',
+      cardinality1: '1',
+      cardinality2: '1'
+    });
+
+    this.sourceEntityElement.connectorActive = false;
+    this.sourceEntityIndex = -1;
   }
 }
 
